1257ef4b55cf53f1d55ca2946336769a4602753e
Saranjeet Bhachu <sanj.s.b@gmail.com>
2015-01-14 01:07:35 +0000

    fixed dao tests

--- FlingChallenge/app/src/test/java/com/sbhachu/flingchallenge/test/data/dao/ImageItemDAOTest.java
+++ FlingChallenge/app/src/test/java/com/sbhachu/flingchallenge/test/data/dao/ImageItemDAOTest.java
@@ -1,32 +1,62 @@
 package com.sbhachu.flingchallenge.test.data.dao;
 
+import android.content.Context;
+
+import com.j256.ormlite.support.ConnectionSource;
+import com.sbhachu.flingchallenge.data.DatabaseHelper;
 import com.sbhachu.flingchallenge.data.dao.ImageItemDAO;
 import com.sbhachu.flingchallenge.data.model.ImageItem;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.fest.assertions.api.Assertions.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 /**
  * Created by sbhachu on 11/01/2015.
  */
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(RobolectricTestRunner.class)
+@Config(emulateSdk = 18)
 public class ImageItemDAOTest {
+
+    @Mock
+    private DatabaseHelper databaseHelper;
+
+    @Mock
+    private ConnectionSource connectionSource;
 
     @Mock
     private ImageItemDAO imageItemDAO;
 
     @Before
     public void setUp() throws Exception {
-        //imageItemDao =
+        initMocks(this);
+        databaseHelper = new DatabaseHelper(Robolectric.application.getApplicationContext());
+        connectionSource = databaseHelper.getConnectionSource();
+        imageItemDAO = new ImageItemDAO(connectionSource, ImageItem.class);
+    }
 
+    @After
+    public void tearDown() throws Exception {
+        imageItemDAO = null;
+
+        connectionSource.close();
+        connectionSource = null;
+
+        databaseHelper.close();
+        databaseHelper = null;
     }
 
     @Test
@@ -61,6 +91,5 @@ public class ImageItemDAOTest {
 
         // Then
         assertThat(count).isEqualTo(3);
-
     }
 }
